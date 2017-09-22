a6a233fadde0199a909e616b368f9a1c4498bcb4
howardmann <howardmann27@gmail.com>
2017-09-22 21:00:31 +1000

    Add unit tests

--- lifx.js
+++ lifx.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
 // Dependencies
-let axios = require('axios')
 let args = require('yargs-parser')(process.argv.slice(2))
+let {
+  switchOn,
+  switchOff,
+  switchColor
+} = require('./switches.js')
 
 // print help
 let printHelp = () => {
@@ -14,43 +18,6 @@ let printHelp = () => {
     --help        print help
   `);
 }
-
-// API and bearer token
-const TOKEN = process.env.LIFX_API_KEY
-let url = "https://api.lifx.com/v1/lights/all/state"
-let config = {
-  headers: { 'Authorization': `Bearer ${TOKEN}` }
-};
-
-let switchOn = () => {
-  let payload = { power: "on"}
-
-  axios.put(url, payload, config).then(results => {
-    console.log(results.data)
-    return results.data
-  })
-}
-
-let switchOff = () => {
-  let payload = { power: "off"}
-
-  axios.put(url, payload, config).then(results => {
-    console.log(results.data)
-    return results.data
-  })
-}
-
-let switchColor = (color) => {
-  let payload = {
-    power: "on",
-    color: color
-  }
-  axios.put(url, payload, config).then(results => {
-    console.log(results.data)
-    return results.data
-  })  
-}
-
 
 let valid = args.on || args.off || args.help || args.color
 
